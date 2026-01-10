e95d3458946147c510d1d9ea33c16d3999689f9b
GGC-svg <dalehuang@gamamobi.com>
2026-01-10 21:51:45 +0800

    chore: Update .gcloudignore and project configurations

--- games/ViperPro-Casino-main/public/originals/bikiniparadise/api/inc/conf.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$res = new stdClass();
-
-$db_host = getenv('DB_HOST') ?: "localhost";
-$db_user = getenv('DB_USERNAME') ?: "root";
-$db_pass = getenv('DB_PASSWORD') ?: "";
-$db_name = getenv('DB_DATABASE') ?: "ggds_casinoprovider";
-
-$con = new mysqli($db_host, $db_user, $db_pass, $db_name);
